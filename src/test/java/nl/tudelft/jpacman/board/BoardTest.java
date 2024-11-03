9c9d3a1d60c5c931d9ab252e20e3664024c0d770
buritbest321 <burit.sih@gmail.com>
2024-11-03 18:33:11 +0700

    Delete Unused import

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -1,7 +1,6 @@
 package nl.tudelft.jpacman.board;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class BoardTest {
