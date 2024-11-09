86242d9c861ac22f859341433987da052ae9ea32
buritbest321 <burit.sih@gmail.com>
2024-11-09 21:26:38 +0700

    Update MapParserTest.java
    Add unit test for MapParser parseMap method with mock verifications

--- src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
+++ src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
@@ -34,13 +34,25 @@ public class MapParserTest {
         MockitoAnnotations.initMocks(this);
         assertNotNull(boardFactory);
         assertNotNull(levelFactory);
+
+        // Setup mock behavior
         Mockito.when(levelFactory.createGhost()).thenReturn(blinky);
         MapParser mapParser = new MapParser(levelFactory, boardFactory);
         ArrayList<String> map = new ArrayList<>();
         map.add("############");
         map.add("#P        G#");
         map.add("############");
+
+        // Act
         mapParser.parseMap(map);
+
+        // Verifications
+        Mockito.verify(levelFactory, Mockito.times(1))
+            .createGhost();
+        Mockito.verify(boardFactory, Mockito.times(1))
+            .createBoard(Mockito.any()); // Verify board creation
+        Mockito.verify(levelFactory, Mockito.times(1))
+            .createLevel(Mockito.any(), Mockito.any(), Mockito.any()); // Verify level creation
     }
 
 }
